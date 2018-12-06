39f8c44994011cf125ba052a887421b4d57558e1
lucas <lucas@192.168.1.181>
2018-12-05 21:07:23 -0500

    Lucas:Started writing the request object class

--- LA7/src/edu/wmich/cs1120/LA7/Request.java
+++ LA7/src/edu/wmich/cs1120/LA7/Request.java
@@ -1,5 +1,42 @@
 package edu.wmich.cs1120.LA7;
 
-public class Request {
-
+public class Request{
+	String studentName;
+	String studentDepartment;
+	String studentLevel;
+	String courseDepartment;
+	int courseNumber;
+	double[][] GPA_Array;
+	
+	
+	public void request(String studentNameIn, String studentDepIn, String studentLevelIn, String courseDeptIn, int courseNumIn, double[][] GPA_ArrayIn) {
+		studentName = studentNameIn;
+		studentDepartment = studentDepIn;
+		studentLevel = studentLevelIn;
+		courseDepartment = courseDeptIn;
+		courseNumber = courseNumIn;
+		GPA_Array = GPA_ArrayIn;
+	}
+	
+	public int yearsFromGarduation(String level) {
+		if(level == "Senior" ) {
+			return 0;
+		}
+		if(level == "Junior") {
+			return 1;
+		}
+		if(level == "Sophomore") {
+			return 2;
+		}
+		if(level == "Freshman") {
+			return 3;
+		}
+		else {
+			return 4;
+		}
+	}
+	
+	public int compareTo(int other) {
+		return 999;
+	}
 }

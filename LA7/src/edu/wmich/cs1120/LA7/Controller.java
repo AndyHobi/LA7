6d0d05efc81380928cf4fac60caa94b5be3707b4
lucas <lucas@192.168.1.181>
2018-12-05 20:43:01 -0500

    Lucas:Finished object creation and saved course objects to a linked list

--- LA7/src/edu/wmich/cs1120/LA7/Controller.java
+++ LA7/src/edu/wmich/cs1120/LA7/Controller.java
@@ -9,12 +9,13 @@ public class Controller implements IController {
 		File file = new File("course.txt");
 		Scanner scan = new Scanner(file);
 		String[] inputArr = new String[3];
+		LinkedList linkList = new LinkedList();
 		while(scan.hasNextLine()) {
 			String input = scan.nextLine();
 			inputArr = input.split(",");
 			Course course = new Course();
 			course.course(inputArr[0],Integer.parseInt(inputArr[1]),Integer.parseInt(inputArr[2]));
-			
+			linkList.addNode(course);
 		}
 	}
 }
